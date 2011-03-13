ca200a181012ae3f5729abc69f15e87c26388dd4
Leonardo Ono <ono.leo@gmail.com>
2011-03-13 20:42:04 -0300

    Implementei e adicionei o registro A110.
    Registro A110 = Complemento do documento - informacao complementar da NF.
    
    Signed-off-by: Leonardo Ono <ono.leo@gmail.com>

---
+++ src/ono/leo/erp/efd/pis_cofins/blocoA/RegistroA110.java
@@ -0,0 +1,49 @@
+package ono.leo.erp.efd.pis_cofins.blocoA;
+
+import ono.leo.erp.efd.pis_cofins.Registro;
+import ono.leo.erp.efd.pis_cofins.Registro.Obrigatoriedade;
+
+/**
+ * Registro A110. 
+ * Complemento do documento - informacao complementar da NF.
+ * 
+ * @author Leonardo Ono (ono.leo@gmail.com)
+ * @since 1.00.00 (13/03/2011 20:31)
+ */
+public class RegistroA110 extends Registro {
+    
+    private String COD_INF = "";
+    private String TXT_COMPL = "";
+    
+    public RegistroA110() {
+        REG = "A110";
+        obrigatoriedade = Obrigatoriedade.OC;
+    }
+
+    public String getCOD_INF() {
+        return COD_INF;
+    }
+
+    public void setCOD_INF(String COD_INF) {
+        this.COD_INF = COD_INF;
+    }
+
+    public String getTXT_COMPL() {
+        return TXT_COMPL;
+    }
+
+    public void setTXT_COMPL(String TXT_COMPL) {
+        this.TXT_COMPL = TXT_COMPL;
+    }
+
+    @Override
+    public String gerarLinha() {
+        String linha = "";
+        linha += "|" + REG;
+        linha += "|" + COD_INF;
+        linha += "|" + TXT_COMPL;
+        linha += "|";
+        return linha;
+    }
+
+}
