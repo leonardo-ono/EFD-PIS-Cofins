d186cf8f8aaa931bf1fa0a7872825defe2450f2b
Leonardo Ono <ono.leo@gmail.com>
2011-03-14 12:59:02 -0300

    Implementei e adicionei o registro C505.
    Registro C505 = Complemento da operacao (codigos 06, 28 e 29) - Cofins.
    
    
    Signed-off-by: Leonardo Ono <ono.leo@gmail.com>

---
+++ src/ono/leo/erp/efd/pis_cofins/blocoC/RegistroC505.java
@@ -0,0 +1,99 @@
+package ono.leo.erp.efd.pis_cofins.blocoC;
+
+import ono.leo.erp.efd.pis_cofins.Registro;
+import ono.leo.erp.efd.pis_cofins.Registro.Obrigatoriedade;
+
+/**
+ * Registro C505.
+ * Complemento da operacao (codigos 06, 28 e 29) - Cofins
+ * 
+ * @author Leonardo Ono (ono.leo@gmail.com)
+ * @since 1.00.00 (14/03/2011 12:50)
+ */
+public class RegistroC505 extends Registro {
+    
+    private String CST_COFINS = "";
+    private String VL_ITEM = "";
+    private String NAT_BC_CRED = "";
+    private String VL_BC_COFINS = "";
+    private String ALIQ_COFINS = "";
+    private String VL_COFINS = "";
+    private String COD_CTA = "";
+ 
+    public RegistroC505() {
+        REG = "C505";
+        obrigatoriedade = Obrigatoriedade.O_SE;
+    }
+
+    public String getALIQ_COFINS() {
+        return ALIQ_COFINS;
+    }
+
+    public void setALIQ_COFINS(String ALIQ_COFINS) {
+        this.ALIQ_COFINS = ALIQ_COFINS;
+    }
+
+    public String getCOD_CTA() {
+        return COD_CTA;
+    }
+
+    public void setCOD_CTA(String COD_CTA) {
+        this.COD_CTA = COD_CTA;
+    }
+
+    public String getCST_COFINS() {
+        return CST_COFINS;
+    }
+
+    public void setCST_COFINS(String CST_COFINS) {
+        this.CST_COFINS = CST_COFINS;
+    }
+
+    public String getNAT_BC_CRED() {
+        return NAT_BC_CRED;
+    }
+
+    public void setNAT_BC_CRED(String NAT_BC_CRED) {
+        this.NAT_BC_CRED = NAT_BC_CRED;
+    }
+
+    public String getVL_BC_COFINS() {
+        return VL_BC_COFINS;
+    }
+
+    public void setVL_BC_COFINS(String VL_BC_COFINS) {
+        this.VL_BC_COFINS = VL_BC_COFINS;
+    }
+
+    public String getVL_COFINS() {
+        return VL_COFINS;
+    }
+
+    public void setVL_COFINS(String VL_COFINS) {
+        this.VL_COFINS = VL_COFINS;
+    }
+
+    public String getVL_ITEM() {
+        return VL_ITEM;
+    }
+
+    public void setVL_ITEM(String VL_ITEM) {
+        this.VL_ITEM = VL_ITEM;
+    }
+
+    @Override
+    public String gerarLinha() {
+        String linha = "";
+        linha += "|" + REG;
+        linha += "|" + CST_COFINS;
+        linha += "|" + VL_ITEM;
+        linha += "|" + NAT_BC_CRED;
+        linha += "|" + VL_BC_COFINS;
+        linha += "|" + ALIQ_COFINS;
+        linha += "|" + VL_COFINS;
+        linha += "|" + COD_CTA;
+        linha += "|";
+        return linha;
+    }
+
+}
