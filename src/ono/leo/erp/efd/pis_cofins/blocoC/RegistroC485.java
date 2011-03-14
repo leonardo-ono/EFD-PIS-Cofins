d170728e1c3336fb5d29289afbc20cda362b1aa8
Leonardo Ono <ono.leo@gmail.com>
2011-03-14 12:21:52 -0300

    Implementei e adicionei o registro C485.
    Registro C485 = Resumo diario de documentos emitidos por ECF - Cofins (codigos 02 e 2D).
    
    
    Signed-off-by: Leonardo Ono <ono.leo@gmail.com>

---
+++ src/ono/leo/erp/efd/pis_cofins/blocoC/RegistroC485.java
@@ -0,0 +1,119 @@
+package ono.leo.erp.efd.pis_cofins.blocoC;
+
+import ono.leo.erp.efd.pis_cofins.Registro;
+import ono.leo.erp.efd.pis_cofins.Registro.Obrigatoriedade;
+
+/**
+ * Registro C485.
+ * Resumo diario de documentos emitidos por ECF - Cofins (codigos 02 e 2D).
+ * 
+ * @author Leonardo Ono (ono.leo@gmail.com)
+ * @since 1.00.00 (14/03/2011 12:20)
+ */
+public class RegistroC485 extends Registro {
+    
+    private String CST_COFINS = "";
+    private String VL_ITEM = "";
+    private String VL_BC_COFINS = "";
+    private String ALIQ_COFINS = "";
+    private String QUANT_BC_COFINS = "";
+    private String ALIQ_COFINS_QUANT = "";
+    private String VL_COFINS = "";
+    private String COD_ITEM = "";
+    private String COD_CTA = "";
+ 
+    public RegistroC485() {
+        REG = "C485";
+        obrigatoriedade = Obrigatoriedade.OC;
+    }
+
+    public String getALIQ_COFINS() {
+        return ALIQ_COFINS;
+    }
+
+    public void setALIQ_COFINS(String ALIQ_COFINS) {
+        this.ALIQ_COFINS = ALIQ_COFINS;
+    }
+
+    public String getALIQ_COFINS_QUANT() {
+        return ALIQ_COFINS_QUANT;
+    }
+
+    public void setALIQ_COFINS_QUANT(String ALIQ_COFINS_QUANT) {
+        this.ALIQ_COFINS_QUANT = ALIQ_COFINS_QUANT;
+    }
+
+    public String getCOD_CTA() {
+        return COD_CTA;
+    }
+
+    public void setCOD_CTA(String COD_CTA) {
+        this.COD_CTA = COD_CTA;
+    }
+
+    public String getCOD_ITEM() {
+        return COD_ITEM;
+    }
+
+    public void setCOD_ITEM(String COD_ITEM) {
+        this.COD_ITEM = COD_ITEM;
+    }
+
+    public String getCST_COFINS() {
+        return CST_COFINS;
+    }
+
+    public void setCST_COFINS(String CST_COFINS) {
+        this.CST_COFINS = CST_COFINS;
+    }
+
+    public String getQUANT_BC_COFINS() {
+        return QUANT_BC_COFINS;
+    }
+
+    public void setQUANT_BC_COFINS(String QUANT_BC_COFINS) {
+        this.QUANT_BC_COFINS = QUANT_BC_COFINS;
+    }
+
+    public String getVL_BC_COFINS() {
+        return VL_BC_COFINS;
+    }
+
+    public void setVL_BC_COFINS(String VL_BC_COFINS) {
+        this.VL_BC_COFINS = VL_BC_COFINS;
+    }
+
+    public String getVL_COFINS() {
+        return VL_COFINS;
+    }
+
+    public void setVL_COFINS(String VL_COFINS) {
+        this.VL_COFINS = VL_COFINS;
+    }
+
+    public String getVL_ITEM() {
+        return VL_ITEM;
+    }
+
+    public void setVL_ITEM(String VL_ITEM) {
+        this.VL_ITEM = VL_ITEM;
+    }
+
+    @Override
+    public String gerarLinha() {
+        String linha = "";
+        linha += "|" + REG;
+        linha += "|" + CST_COFINS;
+        linha += "|" + VL_ITEM;
+        linha += "|" + VL_BC_COFINS;
+        linha += "|" + ALIQ_COFINS;
+        linha += "|" + QUANT_BC_COFINS;
+        linha += "|" + ALIQ_COFINS_QUANT;
+        linha += "|" + VL_COFINS;
+        linha += "|" + COD_ITEM;
+        linha += "|" + COD_CTA;
+        linha += "|";
+        return linha;
+    }
+
+}
