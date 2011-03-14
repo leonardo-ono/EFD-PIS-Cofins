81731fbb74ecee9979139fa4af38f2ca78cc0c99
Leonardo Ono <ono.leo@gmail.com>
2011-03-14 11:44:10 -0300

    Implementei e adicionei o registro C198.
    Registro C198 = Processo referenciado.
    
    
    Signed-off-by: Leonardo Ono <ono.leo@gmail.com>

---
+++ src/ono/leo/erp/efd/pis_cofins/blocoC/RegistroC198.java
@@ -0,0 +1,49 @@
+package ono.leo.erp.efd.pis_cofins.blocoC;
+
+import ono.leo.erp.efd.pis_cofins.Registro;
+import ono.leo.erp.efd.pis_cofins.Registro.Obrigatoriedade;
+
+/**
+ * Registro C198. 
+ * Processo referenciado.
+ * 
+ * @author Leonardo Ono (ono.leo@gmail.com)
+ * @since 1.00.00 (14/03/2011 11:42)
+ */
+public class RegistroC198 extends Registro {
+    
+    private String NUM_PROC = "";
+    private String IND_PROC = "";
+ 
+    public RegistroC198() {
+        REG = "C198";
+        obrigatoriedade = Obrigatoriedade.OC;
+    }
+
+    public String getIND_PROC() {
+        return IND_PROC;
+    }
+
+    public void setIND_PROC(String IND_PROC) {
+        this.IND_PROC = IND_PROC;
+    }
+
+    public String getNUM_PROC() {
+        return NUM_PROC;
+    }
+
+    public void setNUM_PROC(String NUM_PROC) {
+        this.NUM_PROC = NUM_PROC;
+    }
+
+    @Override
+    public String gerarLinha() {
+        String linha = "";
+        linha += "|" + REG;
+        linha += "|" + NUM_PROC;
+        linha += "|" + IND_PROC;
+        linha += "|";
+        return linha;
+    }
+
+}
