062f8aa4c0b5069f0048ff74ac22eb243614ef0a
Leonardo Ono <ono.leo@gmail.com>
2011-03-14 13:28:05 -0300

    Implementei e adicionei o registro C605.
    Registro C605 = Complemento da consolidacao diaria (codigos 06, 28 e 29) -
                    documentos de saidas - Cofins.
    
    
    Signed-off-by: Leonardo Ono <ono.leo@gmail.com>

---
+++ src/ono/leo/erp/efd/pis_cofins/blocoC/RegistroC605.java
@@ -0,0 +1,90 @@
+package ono.leo.erp.efd.pis_cofins.blocoC;
+
+import ono.leo.erp.efd.pis_cofins.Registro;
+import ono.leo.erp.efd.pis_cofins.Registro.Obrigatoriedade;
+
+/**
+ * Registro C605.
+ * Complemento da consolidacao diaria (codigos 06, 28 e 29) -
+ * documentos de saidas - Cofins.
+ * 
+ * @author Leonardo Ono (ono.leo@gmail.com)
+ * @since 1.00.00 (14/03/2011 13:23)
+ */
+public class RegistroC605 extends Registro {
+    
+    private String CST_COFINS = "";
+    private String VL_ITEM = "";
+    private String VL_BC_COFINS = "";
+    private String ALIQ_COFINS = "";
+    private String VL_COFINS = "";
+    private String COD_CTA = "";
+ 
+    public RegistroC605() {
+        REG = "C605";
+        obrigatoriedade = Obrigatoriedade.O_SE;
+    }
+
+    public String getALIQ_COFINS() {
+        return ALIQ_COFINS;
+    }
+
+    public void setALIQ_COFINS(String ALIQ_COFINS) {
+        this.ALIQ_COFINS = ALIQ_COFINS;
+    }
+
+    public String getCOD_CTA() {
+        return COD_CTA;
+    }
+
+    public void setCOD_CTA(String COD_CTA) {
+        this.COD_CTA = COD_CTA;
+    }
+
+    public String getCST_COFINS() {
+        return CST_COFINS;
+    }
+
+    public void setCST_COFINS(String CST_COFINS) {
+        this.CST_COFINS = CST_COFINS;
+    }
+
+    public String getVL_BC_COFINS() {
+        return VL_BC_COFINS;
+    }
+
+    public void setVL_BC_COFINS(String VL_BC_COFINS) {
+        this.VL_BC_COFINS = VL_BC_COFINS;
+    }
+
+    public String getVL_COFINS() {
+        return VL_COFINS;
+    }
+
+    public void setVL_COFINS(String VL_COFINS) {
+        this.VL_COFINS = VL_COFINS;
+    }
+
+    public String getVL_ITEM() {
+        return VL_ITEM;
+    }
+
+    public void setVL_ITEM(String VL_ITEM) {
+        this.VL_ITEM = VL_ITEM;
+    }
+
+    @Override
+    public String gerarLinha() {
+        String linha = "";
+        linha += "|" + REG;
+        linha += "|" + CST_COFINS;
+        linha += "|" + VL_ITEM;
+        linha += "|" + VL_BC_COFINS;
+        linha += "|" + ALIQ_COFINS;
+        linha += "|" + VL_COFINS;
+        linha += "|" + COD_CTA;
+        linha += "|";
+        return linha;
+    }
+
+}
