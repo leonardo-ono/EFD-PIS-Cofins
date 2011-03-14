bc81b566e55c60ee83b27137848f116061e4a725
Leonardo Ono <ono.leo@gmail.com>
2011-03-14 12:49:22 -0300

    Implementei e adicionei o registro C501.
    Registro C501 = Complemento da operacao (codigos 06, 28 e 29) - PIS/PASEP.
    
    
    Signed-off-by: Leonardo Ono <ono.leo@gmail.com>

---
+++ src/ono/leo/erp/efd/pis_cofins/blocoC/RegistroC501.java
@@ -0,0 +1,99 @@
+package ono.leo.erp.efd.pis_cofins.blocoC;
+
+import ono.leo.erp.efd.pis_cofins.Registro;
+import ono.leo.erp.efd.pis_cofins.Registro.Obrigatoriedade;
+
+/**
+ * Registro C501.
+ * Complemento da operacao (codigos 06, 28 e 29) - PIS/PASEP.
+ * 
+ * @author Leonardo Ono (ono.leo@gmail.com)
+ * @since 1.00.00 (14/03/2011 12:46)
+ */
+public class RegistroC501 extends Registro {
+    
+    private String CST_PIS = "";
+    private String VL_ITEM = "";
+    private String NAT_BC_CRED = "";
+    private String VL_BC_PIS = "";
+    private String ALIQ_PIS = "";
+    private String VL_PIS = "";
+    private String COD_CTA = "";
+ 
+    public RegistroC501() {
+        REG = "C501";
+        obrigatoriedade = Obrigatoriedade.O_SE;
+    }
+
+    public String getALIQ_PIS() {
+        return ALIQ_PIS;
+    }
+
+    public void setALIQ_PIS(String ALIQ_PIS) {
+        this.ALIQ_PIS = ALIQ_PIS;
+    }
+
+    public String getCOD_CTA() {
+        return COD_CTA;
+    }
+
+    public void setCOD_CTA(String COD_CTA) {
+        this.COD_CTA = COD_CTA;
+    }
+
+    public String getCST_PIS() {
+        return CST_PIS;
+    }
+
+    public void setCST_PIS(String CST_PIS) {
+        this.CST_PIS = CST_PIS;
+    }
+
+    public String getNAT_BC_CRED() {
+        return NAT_BC_CRED;
+    }
+
+    public void setNAT_BC_CRED(String NAT_BC_CRED) {
+        this.NAT_BC_CRED = NAT_BC_CRED;
+    }
+
+    public String getVL_BC_PIS() {
+        return VL_BC_PIS;
+    }
+
+    public void setVL_BC_PIS(String VL_BC_PIS) {
+        this.VL_BC_PIS = VL_BC_PIS;
+    }
+
+    public String getVL_ITEM() {
+        return VL_ITEM;
+    }
+
+    public void setVL_ITEM(String VL_ITEM) {
+        this.VL_ITEM = VL_ITEM;
+    }
+
+    public String getVL_PIS() {
+        return VL_PIS;
+    }
+
+    public void setVL_PIS(String VL_PIS) {
+        this.VL_PIS = VL_PIS;
+    }
+
+    @Override
+    public String gerarLinha() {
+        String linha = "";
+        linha += "|" + REG;
+        linha += "|" + CST_PIS;
+        linha += "|" + VL_ITEM;
+        linha += "|" + NAT_BC_CRED;
+        linha += "|" + VL_BC_PIS;
+        linha += "|" + ALIQ_PIS;
+        linha += "|" + VL_PIS;
+        linha += "|" + COD_CTA;
+        linha += "|";
+        return linha;
+    }
+
+}
