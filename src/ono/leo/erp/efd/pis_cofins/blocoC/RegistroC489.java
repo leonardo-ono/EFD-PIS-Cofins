df006dc0bca1d091e2b012a8de5c43a474733881
Leonardo Ono <ono.leo@gmail.com>
2011-03-14 12:23:53 -0300

    Implementei e adicionei o registro C489.
    Registro C489 =  Processo referenciado.
    
    
    Signed-off-by: Leonardo Ono <ono.leo@gmail.com>

---
+++ src/ono/leo/erp/efd/pis_cofins/blocoC/RegistroC489.java
@@ -0,0 +1,49 @@
+package ono.leo.erp.efd.pis_cofins.blocoC;
+
+import ono.leo.erp.efd.pis_cofins.Registro;
+import ono.leo.erp.efd.pis_cofins.Registro.Obrigatoriedade;
+
+/**
+ * Registro C489
+ * Processo referenciado.
+ * 
+ * @author Leonardo Ono (ono.leo@gmail.com)
+ * @since 1.00.00 (14/03/2011 12:22)
+ */
+public class RegistroC489 extends Registro {
+    
+    private String NUM_PROC = "";
+    private String IND_PROC = "";
+ 
+    public RegistroC489() {
+        REG = "C489";
+        obrigatoriedade = Obrigatoriedade.OC;
+    }
+
+    public String getIND_PROC() {
+        return IND_PROC;
+    }
+
+    public void setIND_PROC(String IND_PROC) {
+        this.IND_PROC = IND_PROC;
+    }
+
+    public String getNUM_PROC() {
+        return NUM_PROC;
+    }
+
+    public void setNUM_PROC(String NUM_PROC) {
+        this.NUM_PROC = NUM_PROC;
+    }
+
+    @Override
+    public String gerarLinha() {
+        String linha = "";
+        linha += "|" + REG;
+        linha += "|" + NUM_PROC;
+        linha += "|" + IND_PROC;
+        linha += "|";
+        return linha;
+    }
+
+}
