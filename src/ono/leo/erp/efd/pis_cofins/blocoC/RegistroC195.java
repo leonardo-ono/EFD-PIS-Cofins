41e7c687937ae84fa316b273d06be2ad8024af47
Leonardo Ono <ono.leo@gmail.com>
2011-03-14 11:41:59 -0300

    Implementei e adicionei o registro C195.
    Registro C195 = Detalhamento da consolidacao - operacoes de aquisicao com direito a
                    credito, e operacoes de devolucao de compras e vendas - Cofins.
    
    
    Signed-off-by: Leonardo Ono <ono.leo@gmail.com>

---
+++ src/ono/leo/erp/efd/pis_cofins/blocoC/RegistroC195.java
@@ -0,0 +1,140 @@
+package ono.leo.erp.efd.pis_cofins.blocoC;
+
+import ono.leo.erp.efd.pis_cofins.Registro;
+import ono.leo.erp.efd.pis_cofins.Registro.Obrigatoriedade;
+
+/**
+ * Registro C195. 
+ * Detalhamento da consolidacao - operacoes de aquisicao com direito a 
+ * credito, e operacoes de devolucao de compras e vendas - Cofins.
+ * 
+ * @author Leonardo Ono (ono.leo@gmail.com)
+ * @since 1.00.00 (14/03/2011 11:40)
+ */
+public class RegistroC195 extends Registro {
+    
+    private String COD_PART = "";
+    private String CST_COFINS = "";
+    private String CFOP = "";
+    private String VL_ITEM = "";
+    private String VL_DESC = "";
+    private String VL_BC_COFINS = "";
+    private String ALIQ_COFINS = "";
+    private String QUANT_BC_COFINS = "";
+    private String ALIQ_COFINS_QUANT = "";
+    private String VL_COFINS = "";
+    private String COD_CTA = "";
+ 
+    public RegistroC195() {
+        REG = "C195";
+        obrigatoriedade = Obrigatoriedade.O_SE;
+    }
+
+    public String getALIQ_COFINS() {
+        return ALIQ_COFINS;
+    }
+
+    public void setALIQ_COFINS(String ALIQ_COFINS) {
+        this.ALIQ_COFINS = ALIQ_COFINS;
+    }
+
+    public String getALIQ_COFINS_QUANT() {
+        return ALIQ_COFINS_QUANT;
+    }
+
+    public void setALIQ_COFINS_QUANT(String ALIQ_COFINS_QUANT) {
+        this.ALIQ_COFINS_QUANT = ALIQ_COFINS_QUANT;
+    }
+
+    public String getCFOP() {
+        return CFOP;
+    }
+
+    public void setCFOP(String CFOP) {
+        this.CFOP = CFOP;
+    }
+
+    public String getCOD_CTA() {
+        return COD_CTA;
+    }
+
+    public void setCOD_CTA(String COD_CTA) {
+        this.COD_CTA = COD_CTA;
+    }
+
+    public String getCOD_PART() {
+        return COD_PART;
+    }
+
+    public void setCOD_PART(String COD_PART) {
+        this.COD_PART = COD_PART;
+    }
+
+    public String getCST_COFINS() {
+        return CST_COFINS;
+    }
+
+    public void setCST_COFINS(String CST_COFINS) {
+        this.CST_COFINS = CST_COFINS;
+    }
+
+    public String getQUANT_BC_COFINS() {
+        return QUANT_BC_COFINS;
+    }
+
+    public void setQUANT_BC_COFINS(String QUANT_BC_COFINS) {
+        this.QUANT_BC_COFINS = QUANT_BC_COFINS;
+    }
+
+    public String getVL_BC_COFINS() {
+        return VL_BC_COFINS;
+    }
+
+    public void setVL_BC_COFINS(String VL_BC_COFINS) {
+        this.VL_BC_COFINS = VL_BC_COFINS;
+    }
+
+    public String getVL_COFINS() {
+        return VL_COFINS;
+    }
+
+    public void setVL_COFINS(String VL_COFINS) {
+        this.VL_COFINS = VL_COFINS;
+    }
+
+    public String getVL_DESC() {
+        return VL_DESC;
+    }
+
+    public void setVL_DESC(String VL_DESC) {
+        this.VL_DESC = VL_DESC;
+    }
+
+    public String getVL_ITEM() {
+        return VL_ITEM;
+    }
+
+    public void setVL_ITEM(String VL_ITEM) {
+        this.VL_ITEM = VL_ITEM;
+    }
+
+    @Override
+    public String gerarLinha() {
+        String linha = "";
+        linha += "|" + REG;
+        linha += "|" + COD_PART;
+        linha += "|" + CST_COFINS;
+        linha += "|" + CFOP;
+        linha += "|" + VL_ITEM;
+        linha += "|" + VL_DESC;
+        linha += "|" + VL_BC_COFINS;
+        linha += "|" + ALIQ_COFINS;
+        linha += "|" + QUANT_BC_COFINS;
+        linha += "|" + ALIQ_COFINS_QUANT;
+        linha += "|" + VL_COFINS;
+        linha += "|" + COD_CTA;
+        linha += "|";
+        return linha;
+    }
+
+}
