638e4368d41d20e4166a02c1b6435aa4c9d1d05c
Leonardo Ono <ono.leo@gmail.com>
2011-03-14 12:26:01 -0300

    Implementei e adicionei o registro C490.
    Registro C490 = Consolidacao de documentos emitidos por ECF (codigos 02, 2D e 59).
    
    
    Signed-off-by: Leonardo Ono <ono.leo@gmail.com>

---
+++ src/ono/leo/erp/efd/pis_cofins/blocoC/RegistroC490.java
@@ -0,0 +1,58 @@
+package ono.leo.erp.efd.pis_cofins.blocoC;
+
+import ono.leo.erp.efd.pis_cofins.Registro;
+import ono.leo.erp.efd.pis_cofins.Registro.Obrigatoriedade;
+
+/**
+ * Registro C490.
+ * Consolidacao de documentos emitidos por ECF (codigos 02, 2D e 59).
+ * 
+ * @author Leonardo Ono (ono.leo@gmail.com)
+ * @since 1.00.00 (14/03/2011 12:24)
+ */
+public class RegistroC490 extends Registro {
+    
+    private String DT_DOC_INI = "";
+    private String DT_DOC_FIN = "";
+    private String COD_MOD = "";
+ 
+    public RegistroC490() {
+        REG = "C490";
+        obrigatoriedade = Obrigatoriedade.OC;
+    }
+
+    public String getCOD_MOD() {
+        return COD_MOD;
+    }
+
+    public void setCOD_MOD(String COD_MOD) {
+        this.COD_MOD = COD_MOD;
+    }
+
+    public String getDT_DOC_FIN() {
+        return DT_DOC_FIN;
+    }
+
+    public void setDT_DOC_FIN(String DT_DOC_FIN) {
+        this.DT_DOC_FIN = DT_DOC_FIN;
+    }
+
+    public String getDT_DOC_INI() {
+        return DT_DOC_INI;
+    }
+
+    public void setDT_DOC_INI(String DT_DOC_INI) {
+        this.DT_DOC_INI = DT_DOC_INI;
+    }
+
+    @Override
+    public String gerarLinha() {
+        String linha = "";
+        linha += "|" + REG;
+        linha += "|" + DT_DOC_INI;
+        linha += "|" + COD_MOD;
+        linha += "|";
+        return linha;
+    }
+
+}
