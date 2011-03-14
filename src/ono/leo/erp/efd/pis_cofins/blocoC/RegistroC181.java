70ede9982ae3cd7e951ff9c1d426a4d0b55d2d24
Leonardo Ono <ono.leo@gmail.com>
2011-03-14 11:24:36 -0300

    Implementei e adicionei o registro C181.
    Registro C181 = Detalhamento da consolidacao - operacoes de vendas - PIS/PASEP.
    
    
    Signed-off-by: Leonardo Ono <ono.leo@gmail.com>

---
+++ src/ono/leo/erp/efd/pis_cofins/blocoC/RegistroC181.java
@@ -0,0 +1,129 @@
+package ono.leo.erp.efd.pis_cofins.blocoC;
+
+import ono.leo.erp.efd.pis_cofins.Registro;
+import ono.leo.erp.efd.pis_cofins.Registro.Obrigatoriedade;
+
+/**
+ * Registro C181. 
+ * Detalhamento da consolidacao - operacoes de vendas - PIS/PASEP.
+ * 
+ * @author Leonardo Ono (ono.leo@gmail.com)
+ * @since 1.00.00 (14/03/2011 11:17)
+ */
+public class RegistroC181 extends Registro {
+    
+    private String CST_PIS = "";
+    private String CFOP = "";
+    private String VL_ITEM = "";
+    private String VL_DESC = "";
+    private String VL_BC_PIS = "";
+    private String ALIQ_PIS = "";
+    private String QUANT_BC_PIS = "";
+    private String ALIQ_PIS_QUANT = "";
+    private String VL_PIS = "";
+    private String COD_CTA = "";
+ 
+    public RegistroC181() {
+        REG = "C181";
+        obrigatoriedade = Obrigatoriedade.O_SE;
+    }
+
+    public String getALIQ_PIS() {
+        return ALIQ_PIS;
+    }
+
+    public void setALIQ_PIS(String ALIQ_PIS) {
+        this.ALIQ_PIS = ALIQ_PIS;
+    }
+
+    public String getALIQ_PIS_QUANT() {
+        return ALIQ_PIS_QUANT;
+    }
+
+    public void setALIQ_PIS_QUANT(String ALIQ_PIS_QUANT) {
+        this.ALIQ_PIS_QUANT = ALIQ_PIS_QUANT;
+    }
+
+    public String getCFOP() {
+        return CFOP;
+    }
+
+    public void setCFOP(String CFOP) {
+        this.CFOP = CFOP;
+    }
+
+    public String getCOD_CTA() {
+        return COD_CTA;
+    }
+
+    public void setCOD_CTA(String COD_CTA) {
+        this.COD_CTA = COD_CTA;
+    }
+
+    public String getCST_PIS() {
+        return CST_PIS;
+    }
+
+    public void setCST_PIS(String CST_PIS) {
+        this.CST_PIS = CST_PIS;
+    }
+
+    public String getQUANT_BC_PIS() {
+        return QUANT_BC_PIS;
+    }
+
+    public void setQUANT_BC_PIS(String QUANT_BC_PIS) {
+        this.QUANT_BC_PIS = QUANT_BC_PIS;
+    }
+
+    public String getVL_BC_PIS() {
+        return VL_BC_PIS;
+    }
+
+    public void setVL_BC_PIS(String VL_BC_PIS) {
+        this.VL_BC_PIS = VL_BC_PIS;
+    }
+
+    public String getVL_DESC() {
+        return VL_DESC;
+    }
+
+    public void setVL_DESC(String VL_DESC) {
+        this.VL_DESC = VL_DESC;
+    }
+
+    public String getVL_ITEM() {
+        return VL_ITEM;
+    }
+
+    public void setVL_ITEM(String VL_ITEM) {
+        this.VL_ITEM = VL_ITEM;
+    }
+
+    public String getVL_PIS() {
+        return VL_PIS;
+    }
+
+    public void setVL_PIS(String VL_PIS) {
+        this.VL_PIS = VL_PIS;
+    }
+
+    @Override
+    public String gerarLinha() {
+        String linha = "";
+        linha += "|" + REG;
+        linha += "|" + CST_PIS;
+        linha += "|" + CFOP;
+        linha += "|" + VL_ITEM;
+        linha += "|" + VL_DESC;
+        linha += "|" + VL_BC_PIS;
+        linha += "|" + ALIQ_PIS;
+        linha += "|" + QUANT_BC_PIS;
+        linha += "|" + ALIQ_PIS_QUANT;
+        linha += "|" + VL_PIS;
+        linha += "|" + COD_CTA;
+        linha += "|";
+        return linha;
+    }
+
+}
