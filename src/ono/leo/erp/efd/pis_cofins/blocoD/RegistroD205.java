bddd8f4003db0abcca4963bfcc1db6ead05e9df0
Leonardo Ono <ono.leo@gmail.com>
2011-03-21 10:51:12 -0300

    Implementei e adicionei o registro D205.
    Registro D205 = Totalizacao do resumo diario - Cofins.
    
    Signed-off-by: Leonardo Ono <ono.leo@gmail.com>

---
+++ src/ono/leo/erp/efd/pis_cofins/blocoD/RegistroD205.java
@@ -0,0 +1,89 @@
+package ono.leo.erp.efd.pis_cofins.blocoD;
+
+import ono.leo.erp.efd.pis_cofins.Registro;
+import ono.leo.erp.efd.pis_cofins.Registro.Obrigatoriedade;
+
+/**
+ * Registro D205. 
+ * Totalizacao do resumo diario - Cofins.
+ * 
+ * @author Leonardo Ono (ono.leo@gmail.com)
+ * @since 1.00.00 (21/03/2011 10:49)
+ */
+public class RegistroD205 extends Registro {
+    
+    private String CST_COFINS = "";
+    private String VL_ITEM = "";
+    private String VL_BC_COFINS = "";
+    private String ALIQ_COFINS = "";
+    private String VL_COFINS = "";
+    private String COD_CTA = "";
+    
+    public RegistroD205() {
+        REG = "D205";
+        obrigatoriedade = Obrigatoriedade.OC;
+    }
+
+    public String getALIQ_COFINS() {
+        return ALIQ_COFINS;
+    }
+
+    public void setALIQ_COFINS(String ALIQ_COFINS) {
+        this.ALIQ_COFINS = ALIQ_COFINS;
+    }
+
+    public String getCOD_CTA() {
+        return COD_CTA;
+    }
+
+    public void setCOD_CTA(String COD_CTA) {
+        this.COD_CTA = COD_CTA;
+    }
+
+    public String getCST_COFINS() {
+        return CST_COFINS;
+    }
+
+    public void setCST_COFINS(String CST_COFINS) {
+        this.CST_COFINS = CST_COFINS;
+    }
+
+    public String getVL_BC_COFINS() {
+        return VL_BC_COFINS;
+    }
+
+    public void setVL_BC_COFINS(String VL_BC_COFINS) {
+        this.VL_BC_COFINS = VL_BC_COFINS;
+    }
+
+    public String getVL_COFINS() {
+        return VL_COFINS;
+    }
+
+    public void setVL_COFINS(String VL_COFINS) {
+        this.VL_COFINS = VL_COFINS;
+    }
+
+    public String getVL_ITEM() {
+        return VL_ITEM;
+    }
+
+    public void setVL_ITEM(String VL_ITEM) {
+        this.VL_ITEM = VL_ITEM;
+    }
+
+    @Override
+    public String gerarLinha() {
+        String linha = "";
+        linha += "|" + REG;
+        linha += "|" + CST_COFINS;
+        linha += "|" + VL_ITEM;
+        linha += "|" + VL_BC_COFINS;
+        linha += "|" + ALIQ_COFINS;
+        linha += "|" + VL_COFINS;
+        linha += "|" + COD_CTA;
+        linha += "|";
+        return linha;
+    }
+
+}
