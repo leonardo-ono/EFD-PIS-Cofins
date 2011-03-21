3c6921fbe318ec398d59c46d41835a1e58e7ad74
Leonardo Ono <ono.leo@gmail.com>
2011-03-21 10:35:21 -0300

    Implementei e adicionei o registro D105.
    Registro D105 = Complemento do documento de transporte
                    (Codigos 07, 08, 8B, 09, 10, 11, 26, 27 e 57) - Cofins.
    
    
    Signed-off-by: Leonardo Ono <ono.leo@gmail.com>

---
+++ src/ono/leo/erp/efd/pis_cofins/blocoD/RegistroD105.java
@@ -0,0 +1,110 @@
+package ono.leo.erp.efd.pis_cofins.blocoD;
+
+import ono.leo.erp.efd.pis_cofins.Registro;
+import ono.leo.erp.efd.pis_cofins.Registro.Obrigatoriedade;
+
+/**
+ * Registro D105. 
+ * Complemento do documento de transporte 
+ * (Codigos 07, 08, 8B, 09, 10, 11, 26, 27 e 57) - Cofins.
+ * 
+ * @author Leonardo Ono (ono.leo@gmail.com)
+ * @since 1.00.00 (21/03/2011 10:33)
+ */
+public class RegistroD105 extends Registro {
+    
+    private String IND_NAT_FRT = "";
+    private String VL_ITEM = "";
+    private String CST_COFINS = "";
+    private String NAT_BC_CRED = "";
+    private String VL_BC_COFINS = "";
+    private String ALIQ_COFINS = "";
+    private String VL_COFINS = "";
+    private String COD_CTA = "";
+    
+    public RegistroD105() {
+        REG = "D105";
+        obrigatoriedade = Obrigatoriedade.OC;
+    }
+
+    public String getALIQ_COFINS() {
+        return ALIQ_COFINS;
+    }
+
+    public void setALIQ_COFINS(String ALIQ_COFINS) {
+        this.ALIQ_COFINS = ALIQ_COFINS;
+    }
+
+    public String getCOD_CTA() {
+        return COD_CTA;
+    }
+
+    public void setCOD_CTA(String COD_CTA) {
+        this.COD_CTA = COD_CTA;
+    }
+
+    public String getCST_COFINS() {
+        return CST_COFINS;
+    }
+
+    public void setCST_COFINS(String CST_COFINS) {
+        this.CST_COFINS = CST_COFINS;
+    }
+
+    public String getIND_NAT_FRT() {
+        return IND_NAT_FRT;
+    }
+
+    public void setIND_NAT_FRT(String IND_NAT_FRT) {
+        this.IND_NAT_FRT = IND_NAT_FRT;
+    }
+
+    public String getNAT_BC_CRED() {
+        return NAT_BC_CRED;
+    }
+
+    public void setNAT_BC_CRED(String NAT_BC_CRED) {
+        this.NAT_BC_CRED = NAT_BC_CRED;
+    }
+
+    public String getVL_BC_COFINS() {
+        return VL_BC_COFINS;
+    }
+
+    public void setVL_BC_COFINS(String VL_BC_COFINS) {
+        this.VL_BC_COFINS = VL_BC_COFINS;
+    }
+
+    public String getVL_COFINS() {
+        return VL_COFINS;
+    }
+
+    public void setVL_COFINS(String VL_COFINS) {
+        this.VL_COFINS = VL_COFINS;
+    }
+
+    public String getVL_ITEM() {
+        return VL_ITEM;
+    }
+
+    public void setVL_ITEM(String VL_ITEM) {
+        this.VL_ITEM = VL_ITEM;
+    }
+    
+    @Override
+    public String gerarLinha() {
+        String linha = "";
+        linha += "|" + REG;
+        linha += "|" + IND_NAT_FRT;
+        linha += "|" + VL_ITEM;
+        linha += "|" + CST_COFINS;
+        linha += "|" + NAT_BC_CRED;
+        linha += "|" + VL_BC_COFINS;
+        linha += "|" + ALIQ_COFINS;
+        linha += "|" + VL_COFINS;
+        linha += "|" + COD_CTA;
+        linha += "|";
+        return linha;
+    }
+
+}
