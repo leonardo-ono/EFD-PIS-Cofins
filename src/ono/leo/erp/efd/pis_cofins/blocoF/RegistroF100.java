ef056995915bf2f6333a933d8e6d7f52a0e52c63
Leonardo Ono <ono.leo@gmail.com>
2011-03-22 10:52:40 -0300

    Implementei e adicionei o registro F100.
    Registro F100 = Demais documentos e operacoes geradoras de contribuicao e creditos.
    
    Signed-off-by: Leonardo Ono <ono.leo@gmail.com>

---
+++ src/ono/leo/erp/efd/pis_cofins/blocoF/RegistroF100.java
@@ -0,0 +1,209 @@
+package ono.leo.erp.efd.pis_cofins.blocoF;
+
+import ono.leo.erp.efd.pis_cofins.Registro;
+import ono.leo.erp.efd.pis_cofins.Registro.Obrigatoriedade;
+
+/**
+ * Registro F100. 
+ * Demais documentos e operacoes geradoras de contribuicao e creditos.
+ * 
+ * @author Leonardo Ono (ono.leo@gmail.com)
+ * @since 1.00.00 (22/03/2011 10:52)
+ */
+public class RegistroF100 extends Registro {
+    
+    private String IND_OPER = "";
+    private String COD_PART = "";
+    private String COD_ITEM = "";
+    private String DT_OPER = "";
+    private String VL_OPER = "";
+    private String CST_PIS = "";
+    private String VL_BC_PIS = "";
+    private String ALIQ_PIS = "";
+    private String VL_PIS = "";
+    private String CST_COFINS = "";
+    private String VL_BC_COFINS = "";
+    private String ALIQ_COFINS = "";
+    private String VL_COFINS = "";
+    private String NAT_BC_CRED = "";
+    private String IND_ORIG_CRED = "";
+    private String COD_CTA = "";
+    private String COD_CCUS = "";
+    private String DESC_DOC_OPER = "";
+    
+    public RegistroF100() {
+        REG = "F100";
+        obrigatoriedade = Obrigatoriedade.OC;
+    }
+
+    public String getALIQ_COFINS() {
+        return ALIQ_COFINS;
+    }
+
+    public void setALIQ_COFINS(String ALIQ_COFINS) {
+        this.ALIQ_COFINS = ALIQ_COFINS;
+    }
+
+    public String getALIQ_PIS() {
+        return ALIQ_PIS;
+    }
+
+    public void setALIQ_PIS(String ALIQ_PIS) {
+        this.ALIQ_PIS = ALIQ_PIS;
+    }
+
+    public String getCOD_CCUS() {
+        return COD_CCUS;
+    }
+
+    public void setCOD_CCUS(String COD_CCUS) {
+        this.COD_CCUS = COD_CCUS;
+    }
+
+    public String getCOD_CTA() {
+        return COD_CTA;
+    }
+
+    public void setCOD_CTA(String COD_CTA) {
+        this.COD_CTA = COD_CTA;
+    }
+
+    public String getCOD_ITEM() {
+        return COD_ITEM;
+    }
+
+    public void setCOD_ITEM(String COD_ITEM) {
+        this.COD_ITEM = COD_ITEM;
+    }
+
+    public String getCOD_PART() {
+        return COD_PART;
+    }
+
+    public void setCOD_PART(String COD_PART) {
+        this.COD_PART = COD_PART;
+    }
+
+    public String getCST_COFINS() {
+        return CST_COFINS;
+    }
+
+    public void setCST_COFINS(String CST_COFINS) {
+        this.CST_COFINS = CST_COFINS;
+    }
+
+    public String getCST_PIS() {
+        return CST_PIS;
+    }
+
+    public void setCST_PIS(String CST_PIS) {
+        this.CST_PIS = CST_PIS;
+    }
+
+    public String getDESC_DOC_OPER() {
+        return DESC_DOC_OPER;
+    }
+
+    public void setDESC_DOC_OPER(String DESC_DOC_OPER) {
+        this.DESC_DOC_OPER = DESC_DOC_OPER;
+    }
+
+    public String getDT_OPER() {
+        return DT_OPER;
+    }
+
+    public void setDT_OPER(String DT_OPER) {
+        this.DT_OPER = DT_OPER;
+    }
+
+    public String getIND_OPER() {
+        return IND_OPER;
+    }
+
+    public void setIND_OPER(String IND_OPER) {
+        this.IND_OPER = IND_OPER;
+    }
+
+    public String getIND_ORIG_CRED() {
+        return IND_ORIG_CRED;
+    }
+
+    public void setIND_ORIG_CRED(String IND_ORIG_CRED) {
+        this.IND_ORIG_CRED = IND_ORIG_CRED;
+    }
+
+    public String getNAT_BC_CRED() {
+        return NAT_BC_CRED;
+    }
+
+    public void setNAT_BC_CRED(String NAT_BC_CRED) {
+        this.NAT_BC_CRED = NAT_BC_CRED;
+    }
+
+    public String getVL_BC_COFINS() {
+        return VL_BC_COFINS;
+    }
+
+    public void setVL_BC_COFINS(String VL_BC_COFINS) {
+        this.VL_BC_COFINS = VL_BC_COFINS;
+    }
+
+    public String getVL_BC_PIS() {
+        return VL_BC_PIS;
+    }
+
+    public void setVL_BC_PIS(String VL_BC_PIS) {
+        this.VL_BC_PIS = VL_BC_PIS;
+    }
+
+    public String getVL_COFINS() {
+        return VL_COFINS;
+    }
+
+    public void setVL_COFINS(String VL_COFINS) {
+        this.VL_COFINS = VL_COFINS;
+    }
+
+    public String getVL_OPER() {
+        return VL_OPER;
+    }
+
+    public void setVL_OPER(String VL_OPER) {
+        this.VL_OPER = VL_OPER;
+    }
+
+    public String getVL_PIS() {
+        return VL_PIS;
+    }
+
+    public void setVL_PIS(String VL_PIS) {
+        this.VL_PIS = VL_PIS;
+    }
+    
+    @Override
+    public String gerarLinha() {
+        String linha = "";
+        linha += "|" + REG;
+        linha += "|" + IND_OPER;
+        linha += "|" + COD_PART;
+        linha += "|" + COD_ITEM;
+        linha += "|" + DT_OPER;
+        linha += "|" + VL_OPER;
+        linha += "|" + CST_PIS;
+        linha += "|" + VL_BC_PIS;
+        linha += "|" + ALIQ_PIS;
+        linha += "|" + VL_PIS;
+        linha += "|" + CST_COFINS;
+        linha += "|" + VL_BC_COFINS;
+        linha += "|" + ALIQ_COFINS;
+        linha += "|" + VL_COFINS;
+        linha += "|" + NAT_BC_CRED;
+        linha += "|" + IND_ORIG_CRED;
+        linha += "|" + COD_CTA;
+        linha += "|" + COD_CCUS;
+        linha += "|" + DESC_DOC_OPER;
+        linha += "|";
+        return linha;
+    }
+
+}
