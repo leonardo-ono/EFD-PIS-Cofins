6edfa57d47670a243aaee916e7d0f4530052250f
Leonardo Ono <ono.leo@gmail.com>
2011-03-22 11:10:12 -0300

    Implementei e adicionei o registro F139.
    Registro F139 = Processo referenciado.
    
    Signed-off-by: Leonardo Ono <ono.leo@gmail.com>

---
+++ src/ono/leo/erp/efd/pis_cofins/blocoF/RegistroF139.java
@@ -0,0 +1,49 @@
+package ono.leo.erp.efd.pis_cofins.blocoF;
+
+import ono.leo.erp.efd.pis_cofins.Registro;
+import ono.leo.erp.efd.pis_cofins.Registro.Obrigatoriedade;
+
+/**
+ * Registro F139. 
+ * Processo referenciado.
+ * 
+ * @author Leonardo Ono (ono.leo@gmail.com)
+ * @since 1.00.00 (22/03/2011 11:09)
+ */
+public class RegistroF139 extends Registro {
+    
+    private String NUM_PROC = "";
+    private String IND_PROC = "";
+    
+    public RegistroF139() {
+        REG = "F139";
+        obrigatoriedade = Obrigatoriedade.OC;
+    }
+
+    public String getIND_PROC() {
+        return IND_PROC;
+    }
+
+    public void setIND_PROC(String IND_PROC) {
+        this.IND_PROC = IND_PROC;
+    }
+
+    public String getNUM_PROC() {
+        return NUM_PROC;
+    }
+
+    public void setNUM_PROC(String NUM_PROC) {
+        this.NUM_PROC = NUM_PROC;
+    }
+    
+    @Override
+    public String gerarLinha() {
+        String linha = "";
+        linha += "|" + REG;
+        linha += "|" + NUM_PROC;
+        linha += "|" + IND_PROC;
+        linha += "|";
+        return linha;
+    }
+
+}
