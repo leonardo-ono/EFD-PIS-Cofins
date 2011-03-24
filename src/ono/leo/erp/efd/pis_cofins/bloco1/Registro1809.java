2102b74e99a514d8ebfebac8fdf9a046c266c1f1
Leonardo Ono <ono.leo@gmail.com>
2011-03-24 11:41:27 -0300

    Implementei e adicionei o registro 1809.
    Registro 1809 = Processo referenciado.
    
    Signed-off-by: Leonardo Ono <ono.leo@gmail.com>

---
+++ src/ono/leo/erp/efd/pis_cofins/bloco1/Registro1809.java
@@ -0,0 +1,49 @@
+package ono.leo.erp.efd.pis_cofins.bloco1;
+
+import ono.leo.erp.efd.pis_cofins.Registro;
+import ono.leo.erp.efd.pis_cofins.Registro.Obrigatoriedade;
+
+/**
+ * Registro 1809. 
+ * Processo referenciado.
+ * 
+ * @author Leonardo Ono (ono.leo@gmail.com)
+ * @since 1.00.00 (24/03/2011 11:40)
+ */
+public class Registro1809 extends Registro {
+
+    private String NUM_PROC = "";
+    private String IND_PROC = "";    
+    
+    public Registro1809() {
+        REG = "1809";
+        obrigatoriedade = Obrigatoriedade.OC;
+    }
+
+    public String getIND_PROC() {
+        return IND_PROC;
+    }
+
+    public void setIND_PROC(String IND_PROC) {
+        this.IND_PROC = IND_PROC;
+    }
+
+    public String getNUM_PROC() {
+        return NUM_PROC;
+    }
+
+    public void setNUM_PROC(String NUM_PROC) {
+        this.NUM_PROC = NUM_PROC;
+    }
+    
+    @Override
+    public String gerarLinha() {
+        String linha = "";
+        linha += "|" + REG;
+        linha += "|" + NUM_PROC;
+        linha += "|" + IND_PROC;
+        linha += "|";
+        return linha;
+    }
+
+}
