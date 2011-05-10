2435beed5924dae48e4b47152d7b15307dbd439a
Leonardo Ono <ono.leo@gmail.com>
2011-05-10 09:23:13 -0300

    Implementei e adicionei o registro 0450.
    Registro 0450 = Tabela de informacao complementar do documento fiscal.
    
    Havia esquecido de implementar e incluir nos commits anteriores.
    
    
    Signed-off-by: Leonardo Ono <ono.leo@gmail.com>

---
+++ src/ono/leo/erp/efd/pis_cofins/bloco0/Registro0450.java
@@ -0,0 +1,49 @@
+package ono.leo.erp.efd.pis_cofins.bloco0;
+
+import ono.leo.erp.efd.pis_cofins.Registro;
+import ono.leo.erp.efd.pis_cofins.Registro.Obrigatoriedade;
+
+/**
+ * Registro 0450. 
+ * Tabela de informacao complementar do documento fiscal.
+ * 
+ * @author Leonardo Ono (ono.leo@gmail.com)
+ * @since 1.00.00 (10/05/2011 09:06)
+ */public class Registro0450 extends Registro {
+
+    private String COD_INF = "";
+    private String TXT = "";
+
+    public Registro0450() {
+        REG = "0450";
+        nivel = 3;
+        obrigatoriedade = Obrigatoriedade.OC;
+    }
+
+    public String getCOD_INF() {
+        return COD_INF;
+    }
+
+    public void setCOD_INF(String COD_INF) {
+        this.COD_INF = COD_INF;
+    }
+
+    public String getTXT() {
+        return TXT;
+    }
+
+    public void setTXT(String TXT) {
+        this.TXT = TXT;
+    }
+
+    @Override
+    public String gerarLinha() {
+        String linha = "";
+        linha += "|" + REG;
+        linha += "|" + COD_INF;
+        linha += "|" + TXT;
+        linha += "|";
+        return linha;
+    }
+    
+}
