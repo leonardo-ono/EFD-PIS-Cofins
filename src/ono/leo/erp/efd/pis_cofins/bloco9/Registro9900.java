03b60e65f186723561aa9a210773443a545c04b8
Leonardo Ono <ono.leo@gmail.com>
2011-05-17 10:35:25 -0300

    Inclui novamente o comentario na classe registro 9900.
    No NetBeans 7.0 Beta 2, tem um erro que ao utilizar a opcao
    [Codigo-fonte > Inserir codigo > getter e Getter ] acaba
    excluindo o comentario da classe.
    
    
    Signed-off-by: Leonardo Ono <ono.leo@gmail.com>

--- src/ono/leo/erp/efd/pis_cofins/bloco9/Registro9900.java
+++ src/ono/leo/erp/efd/pis_cofins/bloco9/Registro9900.java
@@ -3,7 +3,13 @@ package ono.leo.erp.efd.pis_cofins.bloco9;
 import ono.leo.erp.efd.pis_cofins.Registro;
 import ono.leo.erp.efd.pis_cofins.Registro.Obrigatoriedade;
 
-
+/**
+ * Registro 9900. 
+ * Registros do arquivo.
+ * 
+ * @author Leonardo Ono (ono.leo@gmail.com)
+ * @since 1.00.00 (03/03/2011 12:13)
+ */
 public class Registro9900 extends Registro {
     
     private String REG_BLC = "XXXX";
