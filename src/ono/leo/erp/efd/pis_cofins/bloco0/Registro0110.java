40e224607b5dd0035948bed57ace77a27db9dbed
Leonardo Ono <ono.leo@gmail.com>
2012-09-06 10:21:16 -0300

    No Registro 0110 foi adicionado o atributo IND_REG_CUM.
    Notificado por Evandro schultz.
    
    
    Signed-off-by: Leonardo Ono <ono.leo@gmail.com>

--- src/ono/leo/erp/efd/pis_cofins/bloco0/Registro0110.java
+++ src/ono/leo/erp/efd/pis_cofins/bloco0/Registro0110.java
@@ -15,6 +15,7 @@ public class Registro0110 extends Registro {
     private String COD_INC_TRIB = "1";
     private String IND_APRO_CRED = "1";
     private String COD_TIPO_CONT = "1";
+    private String IND_REG_CUM = "9";
     
     public Registro0110() {
         REG = "0110";
@@ -48,6 +49,14 @@ public class Registro0110 extends Registro {
         this.IND_APRO_CRED = IND_APRO_CRED;
     }
 
+    public String getIND_REG_CUM() {
+        return IND_REG_CUM;
+    }
+
+    public void setIND_REG_CUM(String IND_REG_CUM) {
+        this.IND_REG_CUM = IND_REG_CUM;
+    }
+
     @Override
     public String gerarLinha() {
         String linha = "";
@@ -55,6 +64,7 @@ public class Registro0110 extends Registro {
         linha += "|" + COD_INC_TRIB;
         linha += "|" + IND_APRO_CRED;
         linha += "|" + COD_TIPO_CONT;
+        linha += "|" + IND_REG_CUM;
         linha += "|" + (char) 13 + (char) 10;
         linha += super.gerarLinha();
         return linha;
